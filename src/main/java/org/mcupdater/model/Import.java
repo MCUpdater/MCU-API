d0508600fbedb3df6e14f4c99427ef023714688a
Scott M. Barbour <smbarbour@gmail.com>
2014-03-07 11:42:58 -0600

    FastPack! A CLI pack builder for MCU3!

---
+++ src/main/java/org/mcupdater/model/Import.java
@@ -0,0 +1,28 @@
+package org.mcupdater.model;
+
+public class Import
+{
+	private String url = "";
+	private String serverId;
+
+	public Import(String url, String serverId) {
+		this.url = url;
+		this.serverId = serverId;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getServerId() {
+		return serverId;
+	}
+
+	public void setServerId(String serverId) {
+		this.serverId = serverId;
+	}
+}
